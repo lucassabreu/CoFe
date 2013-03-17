a63019b824d455ceb70e4fda762230f8cdd459bb
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2013-03-17 17:06:28 -0300

    Removing

--- document.sh
+++
@@ -1,1 +0,0 @@
-phpdoc.php -d . -t public/docs/ -i *.config.php,*.data.php,*zend*,*/test/*,*ApplicationTest*,vendor/*,*.phtml,global.php,local.php,Module.php,public/* --title CoFe --sourcecode --markers "TODO"
